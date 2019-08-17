d7cb7e394f315ef99482d23350e2777557510280
akshatgit <akshat14132@iiitd.ac.in>
2019-08-17 20:22:01 +0000

    Initial setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,12 @@ import PropTypes from 'prop-types'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <span className="icon fa-terminal"></span>
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
-            </div>
+                <h1>Akshat Sinha</h1>
+                <p>A Computer Geek with interest in photography, sports and trekking.</p></div>
         </div>
         <nav>
             <ul>
