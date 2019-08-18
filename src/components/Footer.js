10f656c25a65f02d86a20302d844bd7023381ab1
Akshat Sinha <akshat14132@iiitd.ac.in>
2019-08-18 12:07:54 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <p className="copyright"> Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a><br>
         Forked from: <a href="https://github.com/codebushi/gatsby-starter-dimension">Github</a></p>
-        <br>
         <ul className="icons">
             <li><a href="https://www.linkedin.com/in/akshat-sinha-568765167/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
             <li><a href="https://www.facebook.com/akshat.sinha.5095" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
