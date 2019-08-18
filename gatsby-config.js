cdef03d8ec3fe75fe58cb6bd86ba3b4024092cde
Akshat Sinha <akshat14132@iiitd.ac.in>
2019-08-18 11:54:34 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Akshat Sinha",
+    author: "Akshat Sinha",
+    description: "Personal Website: Akshat Sinha"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Akshat Sinha',
+        short_name: 'site',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
