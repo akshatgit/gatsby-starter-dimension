f0c7b32439b52edfb5bb9d1b92a330336f0fbac1
Akshat Sinha <akshat14132@iiitd.ac.in>
2019-08-18 13:37:42 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -66,9 +66,8 @@ class Main extends React.Component {
         </article>
           <article id="resume" className={`${this.props.article === 'resume' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Resume</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <iframe src="../pdf/resume.pdf" width="100%" height="500px">
+          </iframe>
           {close}
         </article>
         {close}
